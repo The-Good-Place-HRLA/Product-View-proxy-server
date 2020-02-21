860b2af75254348144c31b802cf6151fb2850bb7
Brian Price <brianprice88@gmail.com>
2020-02-20 16:23:56 -0800

    "Set up reverse proxy for only my service."

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const request = require('request');
 
 const app = express();
 const port = 3000;
@@ -13,6 +14,6 @@ app.use(morgan('dev'));
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.use('/:id', express.static(path.join(__dirname, '../client/dist')));
+app.get('/:productId', express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => console.log(`\u001b[1;34m Server listening on port ${port}`));
