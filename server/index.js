8944a0495fbc5cff1f955fc843a3bc73846bc8b3
Brian Price <brianprice88@gmail.com>
2020-02-29 10:29:23 -0800

    "Add Redis caching for product id queries"

--- server/index.js
+++ server/index.js
@@ -4,23 +4,40 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const redis = require('redis');
 
 const options = {
     target: 'http://localhost:3333',
-    changeOrigin: true
+    changeOrigin: true,
+    onProxyRes: function (proxyRes, req, res) {
+        var productId = req.params['0'].slice(3) // product id number
+        proxyRes.on('data', function (data) {
+            client.setex(productId, 3600, data.toString('utf-8'))
+        })
+    }
 }
 
 const Proxy = createProxyMiddleware(options)
 
 const app = express();
 const port = 3000;
-
+const REDIS_PORT = 6379;
+const client = redis.createClient(REDIS_PORT);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cors());
 
-app.use('/*', Proxy)
+function checkCache(req, res, next) {
+    var productId = req.params['0'].slice(3) // gets the product id number
+    client.get(productId, (err, data) => {
+        if (err) throw err;
+        if (data !== null) { res.header(200).send(JSON.parse(data)) }
+        else { next() }
+    })
+}
+
+app.use('/*', checkCache, Proxy)
 // app.use(express.static(path.join(__dirname, '../client/dist')));
 // app.get('/:productId', express.static(path.join(__dirname, '../client/dist')));
 
